486028eeb62793d904ddc6a3c9652bef3aa6f396
medeinedapkev <medeine.dapkev@gmail.com>
2023-06-26 21:44:47 +0300

    responsive

--- src/Components/BookPageComponents/BookPageItem/BookPageItem.js
+++ src/Components/BookPageComponents/BookPageItem/BookPageItem.js
@@ -5,41 +5,45 @@ import AdministratorButton from '../../AdministratorButton/AdministratorButton';
 const BookPageItem = ({ data }) => {
 
   return (
-
       <Card body>
         <div className={styles.bookWrapper}>
           <div className={styles.photoWrapper}>
             <img className={styles.bookPhoto} src={data.photo.url} alt={data.photo.title} />
           </div>
 
-          <div className={styles.bookItem}>
-            <div className={styles.administratorButtonWrapper}>
-              <span className={styles.bookAuthor}>{data.author.name}</span>
+          <div className={styles.bookMainInfo}>
 
-              <AdministratorButton 
+              <div className={styles.administratorButtonWrapper}>
+                <span className={styles.bookAuthor}>{data.author.name}</span>
+
+                <AdministratorButton 
+                size="sm"
                 toCreate='/books/create' 
                 toEdit={`/books/edit/${data.id}`} 
                 Delete={`/books/${data.id}`}
                 navigateTo='/books'
                 deleteToast='Book was successfully deleted'
-              />
-            </div>
+                />
+              </div>
 
-            <h2 className={styles.bookTitle}>{data.title}</h2>
+              <div className={styles.bookInfo}>
+                <h2 className={styles.bookTitle}>{data.title}</h2>
 
-            <ul className={styles.bookInfo}>
-              <li><span className={styles.listTitles}>Žanras:</span> {data.category.title}</li>
-              <li><span className={styles.listTitles}>Leidėjas:</span> {data.publisher}</li>
-              <li><span className={styles.listTitles}>Išleidimo metai:</span> {data.year}</li>
-              <li><span className={styles.listTitles}>Puslapiai:</span> {data.pages}</li>
-            </ul>
+                <ul className={styles.bookListInfo}>
+                  <li><span className={styles.listTitles}>Žanras:</span> {data.category.title}</li>
+                  <li><span className={styles.listTitles}>Leidėjas:</span> {data.publisher}</li>
+                  <li><span className={styles.listTitles}>Išleidimo metai:</span> {data.year}</li>
+                  <li><span className={styles.listTitles}>Puslapiai:</span> {data.pages}</li>
+                </ul>
+              </div>
 
-            <p className={styles.bookDescription}>{data.description}</p>
           </div>
 
+          <div className={styles.bookDescriptionWrapper}>
+              <p className={styles.bookDescription}>{data.description}</p>
+          </div>
         </div>
       </Card>
-
   )
 }
 
